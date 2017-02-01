5fb465c6338be652c71af26a09a42516bb4fb5c0
maverick110 <balajirdk@gmail.com>
2017-02-01 12:29:01 +0530

    Add files via upload
    Its possible to Overload the main method in java

---
+++ MainOverload.java
@@ -0,0 +1,27 @@
+
+package passion;
+public class MainOverload {
+  public static final void main(int a)
+  {
+      System.out.println("in Integer main" +a);
+      main(2.3f);
+  }
+  public static final void main(float x)
+  {
+      System.out.println("in Float main"+x);
+      main(2);
+  }
+  public static final void main(float x,int h)
+  {
+      System.out.print(x+"overloaded main"+h);
+  }
+  public static final void main(String rofl[])
+  {
+      main(2);
+      main(7.6f);
+      main(3.3f,7);
+  }
+  
+
+
+}
